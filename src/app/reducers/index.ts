134e11fbbca406db43b18a3afd8d558cf351eab7
c.illies <c.illies@finatix.de>
2021-09-30 09:10:36 +0200

    Add smallest possible counter actions/reducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,4 +1,5 @@
 import {
+  Action,
   ActionReducer,
   ActionReducerMap,
   createFeatureSelector,
@@ -7,12 +8,29 @@ import {
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
 
+// global App State
 export interface State {
-
+  countState: CountState;
 }
 
-export const reducers: ActionReducerMap<State> = {
+// Feature states
+export interface CountState {
+  current: number;
+}
 
+// single reducer that catched multiple actions at once
+const countReducer: ActionReducer<CountState, Action> = (featureState, action) => {
+  const type = action.type;
+  let current = featureState?.current ?? 0;
+  switch(type) {
+    case '+': current += 1; break;
+    case '-': current -= 1; break;
+  }
+  return { ...featureState, current };
+};
+
+export const reducers: ActionReducerMap<State> = {
+  countState: countReducer
 };
 
 
