7b38dc0f00d0fd26e2c8dc6b88e270f8e9f51139
c.illies <c.illies@finatix.de>
2021-10-07 10:17:23 +0200

    Add sample test for effect

--- src/app/reducers/pizza/effect.service.spec.ts
+++ src/app/reducers/pizza/effect.service.spec.ts
@@ -1,8 +1,10 @@
 import {TestBed} from "@angular/core/testing";
-import {PizzaEffectService} from "./effect.service";
+import {LOCAL_STORAGE_KEY, PizzaEffectService} from "./effect.service";
 import {provideMockActions} from "@ngrx/effects/testing";
 import {Subject} from "rxjs";
 import {provideMockStore} from "@ngrx/store/testing";
+import * as fromPizzaActions from "./actions";
+import {Pizza} from "./reducer";
 
 let actions$$: Subject<any>;
 let service: PizzaEffectService;
@@ -20,6 +22,28 @@ beforeEach(() => {
    service = TestBed.inject(PizzaEffectService);
 });
 
+afterEach(() => localStorage.clear());
+
 test('PizzaEffectService', () => {
    expect(service).toBeDefined();
+});
+
+test('read$ - should read pizzen from storage', done => {
+   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify([
+      {
+         name: 'Pizza Salami',
+         price: 11.99,
+         ingridients: ['Zucker'],
+      }
+   ] as Pizza[]));
+
+   service.read$.subscribe(readSuccessAction => {
+      expect(readSuccessAction.type).toEqual(fromPizzaActions.PizzaAction.READ_SUCCESS);
+      expect(readSuccessAction.list).toBeDefined();
+      expect(readSuccessAction.list.length).toEqual(1);
+      expect(readSuccessAction.list[0].name).toEqual('Pizza Salami');
+      done();
+   });
+
+   actions$$.next(fromPizzaActions.read());
 });
